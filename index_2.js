3471a6ff3e7b04714543a7398432db7b2a21718c
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-05-05 23:10:36 -0300

    Tratamento de Erros

--- index_2.js
+++ index_2.js
@@ -1,9 +1,22 @@
 const chalk = require('chalk')
 const fs = require('fs');
 
+//Função para tratar o erro, o throw ira lançar o Erro pelo objeto Error no parametro erro, que sera o dado recebido no readFile
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretorio.'));//Mensagem de Erro opcional apenas para saber onde o erro foi encontrado em Portugues
+}
+
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+            //Parece confuso a quantidade de vezes que erro se repete, mas funciona da
+            //seguinte maneira, o erro chega atraves do parametro passado no readfile, é 
+            //armazenado nele, então passa para a ser verificado no if, e se for true entra
+            //no laço e executa a função erro passando o parametro erro que é o dado armazenado
+            //do readFile
+        }
         console.log(chalk.green(texto));
     });
 }
